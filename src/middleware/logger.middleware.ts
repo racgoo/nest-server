53f34a0bd39ec36ca49cfecaf970c6415e9b6061
성락현 <racgoo@racgoo-mac.local>
2023-11-26 14:43:44 +0900

    test

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -12,7 +12,6 @@ import * as moment from 'moment';
   @Injectable()
   export class LoggerMiddleware implements NestMiddleware {
     constructor(@Inject(Logger) private readonly logger: LoggerService) {}
-  
     use(req: Request, res: Response, next: NextFunction) {
       // 요청 객체로부터 ip, http method, url, user agent를 받아온 후
       const { ip, method, originalUrl } = req;
